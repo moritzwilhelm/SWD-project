cc6b85e85c0916e2a23ae4c15cdcac940518e897
Daniel.Emmel <s8daemme@stud.uni-saarland.de>
2020-08-26 20:37:40 +0200

    [P1] added root redirect

--- simple_ecommerce/simple_ecommerce/urls.py
+++ simple_ecommerce/simple_ecommerce/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     path('', include('user_mgmt.urls'))
 ]
 
