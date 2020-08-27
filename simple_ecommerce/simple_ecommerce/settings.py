02e72a6c419e26998e20f23ea29d4b047caf5902
Moritz Wilhelm <s8mowilh@stud.uni-saarland.de>
2020-08-27 19:24:20 +0200

    [P1] moved base.html

--- simple_ecommerce/simple_ecommerce/settings.py
+++ simple_ecommerce/simple_ecommerce/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'simple_ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'simple_ecommerce/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
