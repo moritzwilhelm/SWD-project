729dc8915efd8cd301c7e29d827a99dd6d5622e3
Moritz Wilhelm <s8mowilh@stud.uni-saarland.de>
2020-09-04 00:22:46 +0200

    [P5] implemented private profile

--- simple_ecommerce/profile/views.py
+++ simple_ecommerce/profile/views.py
@@ -1,12 +1,19 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_GET
+
+from shop.models import Order
 
 
 @login_required()
 @require_GET
 def private(request, email):
-    return render(request, 'profile/private.html')
+    if request.user.username == email:
+        orders = Order.objects.filter(customer_id=request.user, placed=True).order_by("-date_placed", "-id")
+        return render(request, 'profile/private.html', context={'orders': orders})
+    else:
+        return HttpResponse()
 
 
 @login_required()
